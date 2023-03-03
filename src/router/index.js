28fe665a743b7c80b962000b3022e17259e0b607
ausprouty <bob.prouty@powertochange.org.au>
2023-03-03 11:40:09 +1100

    updating index and variables

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,46 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import notFound from "@/views/NotFound.vue";
 // see    https://stackoverflow.com/questions/46590760/vue-js-2-multiple-routing-files
-/*import routesEngHope from "./routesEngHope.js";
+import routesEngHope from "./routesEngHope.js";
 import routesEngMultiply1 from "./routesEngMultiply1.js";
 import routesEngMultiply2 from "./routesEngMultiply2.js";
 import routesEngMultiply3 from "./routesEngMultiply3.js";
 import routesEngProphet from "./routesEngProphet.js";
 import routesEngTc from "./routesEngTc.js";
-*/
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: "/",
-      name: "notfound",
-      component: notFound,
-    }
-/*
     ...routesEngHope,
     ...routesEngMultiply1,
     ...routesEngMultiply2,
     ...routesEngMultiply3,
     ...routesEngProphet,
     ...routesEngTc,
-
+    {
+      path: "/",
+        name: "home",
+        component: function () {
+          return import(
+            "../views/M2/eng/index.vue"
+          );
+        }
+    },
     {
       path: "/index",
         name: "eng-index",
         component: function () {
           return import(
-            / webpackChunkName: "prototype" / "../views/M2/eng/index.vue"
+            "../views/M2/eng/index.vue"
           );
         }
     },
-
-
     {
         path: "/M2/eng/seek",
         name: "eng-seek-index",
         component: function () {
           return import(
-             webpackChunkName: "prototype"  "../views/M2/eng/seek.vue"
+              "../views/M2/eng/seek.vue"
           );
         },
     },
@@ -50,7 +49,6 @@ const router = createRouter({
     name: "notfound",
     component: notFound,
     }
-    */
   ],
 });
 
